d42a6f3d4409cd07d612b2dcb5b44153ebc7d0ea
Chris Martin <christopher.martin@flhealth.gov>
2025-01-31 15:44:24 -0600

    Testing something

---
+++ Echelon-Bot/Echelon-Bot/Modules/ScheduleModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace EchelonBot
+{
+    public class ScheduleModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("mythic", "Schedule a Mythic+")]
+        public Task Mythic(string text)
+            => RespondAsync(text);
+
+        [SlashCommand("raid", "Schedule a Raid")]
+        public Task Raid(string text)
+            => RespondAsync(text);
+    }
+}
