20598cc225d813411173f297affd3f28beb5c701
Chris Martin <christopher.martin@flhealth.gov>
2025-01-31 15:57:16 -0600

    Slash commands take datetime input!!

--- Echelon-Bot/Echelon-Bot/Modules/ScheduleModule.cs
+++ Echelon-Bot/Echelon-Bot/Modules/ScheduleModule.cs
@@ -1,18 +1,19 @@
-﻿using Discord;
-using Discord.Interactions;
-using Discord.WebSocket;
-using System.Threading.Tasks;
+﻿using Discord.Interactions;
 
 namespace EchelonBot
 {
     public class ScheduleModule : InteractionModuleBase<SocketInteractionContext>
     {
         [SlashCommand("mythic", "Schedule a Mythic+")]
-        public Task Mythic(string text)
-            => RespondAsync(text);
+        public Task Mythic(DateTime time, string name)
+        {
+            return RespondAsync($"Raid {name} scheduled for {time}.");
+        }
 
         [SlashCommand("raid", "Schedule a Raid")]
-        public Task Raid(string text)
-            => RespondAsync(text);
+        public Task Raid(DateTime time, string name)
+        {
+            return RespondAsync($"Raid {name} scheduled for {time}.");
+        }
     }
 }
