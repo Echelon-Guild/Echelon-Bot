dca586e58a8bc438b5e4d0566aca1e800cf8e8a1
Chris Martin <christopher.martin@flhealth.gov>
2025-02-02 13:25:57 -0600

    Something weird this way comes

---
+++ Echelon-Bot/Echelon-Bot/Models/Entities/AttendeeRecordEntity.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace EchelonBot.Models.Entities
+{
+    public class AttendeeRecordEntity : ITableEntity
+    {
+        public string PartitionKey { get; set; }  // Event ID
+        public string RowKey { get; set; }  // Unique id per attendee record
+        public string DiscordName { get; set; }
+        public string DiscordDisplayName { get; set; }
+        public string Role { get; set; }
+        public string? Class { get; set; }
+        public string? Spec { get; set; }
+
+        public ETag ETag { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+
+}
---
+++ Echelon-Bot/Echelon-Bot/Models/Entities/EchelonEventEntity.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace EchelonBot.Models.Entities
+{
+    public class EchelonEventEntity : ITableEntity
+    {
+        public string PartitionKey { get; set; }  // E.g., "Raid", "Mythic", "Meeting"
+        public string RowKey { get; set; }  // Unique event ID
+        public string EventName { get; set; }
+        public DateTimeOffset EventDateTime { get; set; }
+        public string Organizer { get; set; }
+        public ulong MessageId { get; set; }
+
+        public ETag ETag { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+
+}
