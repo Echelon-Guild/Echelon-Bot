e0c6e5f7e2e50225bffc705a99c10e6327806e42
Chris Martin <christopher.martin@flhealth.gov>
2025-02-02 01:25:49 -0600

    Added build-and-deploy.yml which should start CI/CD

---
+++ Echelon-Bot/Echelon-Bot/Models/AttendeeRecord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchelonBot.Models
+{
+    public class AttendeeRecord
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        [StringLength(32)]
+        public string DiscordName { get; set; }
+        public string DiscordDisplayName { get; set; }
+        public string Role { get; set; }
+        [StringLength(16)]
+        public string? Class { get; set; }
+        [StringLength(16)]
+        public string? Spec { get; set; }
+
+        public AttendeeRecord() { }
+
+        public AttendeeRecord(int eventId, string discordName, string discordDisplayName, string role, string _class, string spec)
+        {
+            EventId = eventId;
+            DiscordName = discordName;
+            DiscordDisplayName = discordDisplayName;
+            Role = role;
+            Class = _class;
+            Spec = spec;
+        }
+
+        public AttendeeRecord(int eventId, string discordName, string discordDisplayName, string role)
+        {
+            EventId = eventId;
+            DiscordName = discordName;
+            DiscordDisplayName = discordDisplayName;
+            Role = role;
+        }
+
+
+    }
+}
---
+++ Echelon-Bot/Echelon-Bot/Models/EchelonEvent.cs
@@ -0,0 +1,26 @@
+﻿namespace EchelonBot.Models
+{
+    public class EchelonEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset EventDateTime { get; set; }
+        public EventType EventType { get; set; }
+
+        public EchelonEvent() { }
+
+        public EchelonEvent(string name, DateTimeOffset eventDateTime, EventType eventType)
+        {
+            Name = name;
+            EventDateTime = eventDateTime;
+            EventType = eventType;
+        }
+    }
+
+    public enum EventType
+    {
+        Raid,
+        Dungeon,
+        Meeting
+    }
+}
